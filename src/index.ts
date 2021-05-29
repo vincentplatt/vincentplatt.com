fe1975f07c81f38a696bee6fc13d8479b1a9bfd2
Vincent Platt <contact@vincentplatt.com>
2021-05-29 22:03:59 +0100

    Tidy up.

--- src/index.ts
+++ src/index.ts
@@ -28,16 +28,7 @@ window.onload = () => {
 };
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js", { scope: "/" }).then(function(reg) {
-        if (reg.installing) {
-            console.log("Service worker installing");
-        } else if (reg.waiting) {
-            console.log("Service worker installed");
-        } else if (reg.active) {
-            console.log("Service worker active");
-        }
-
-    }).catch(function(error) {
-        console.log("Registration failed with " + error);
+    navigator.serviceWorker.register("service-worker.js", { scope: "/" }).catch(function(error) {
+        console.error("Service worker failed: " + error);
     });
 }
