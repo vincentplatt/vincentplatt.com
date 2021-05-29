d6b58037c3988b44ba38511fe4ca8eb4e2a618ba
Vincent Platt <contact@vincentplatt.com>
2021-05-29 21:33:47 +0100

    Working.

--- src/public/sw.js
+++ src/public/sw.js
@@ -11,25 +11,47 @@ self.addEventListener("install", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-        // caches.match() always resolves
-        // but in case of success response will have value
-        if (response !== undefined) {
-            console.log("Not undefined");
-            return response;
-        } else {
-            console.log("else");
-            return fetch(event.request).then(function(response) {
-                // response may be used only once
-                // we need to save clone to put one copy in cache
-                // and serve second one
-                const responseClone = response.clone();
+    event.respondWith(fetch(event.request).then(res => {
+        console.log("here");
 
-                caches.open(VERSION).then(function(cache) {
-                    cache.put(event.request, responseClone);
-                });
+        const responseClone = res.clone();
+
+        caches.open(VERSION).then(function(cache) {
+            cache.put(event.request, responseClone);
+        });
+        return res;
+    }).catch(error => {
+        console.log("error");
+
+        return caches.match(event.request).then(response => {
+            //     // caches.match() always resolves
+            //     // but in case of success response will have value
+            if (response !== undefined) {
+                console.log("Not undefined");
                 return response;
-            });
-        }
+            }
+            throw error;
+        });
     }));
 });
+
+// caches.match(event.request).then(function(response) {
+//     // caches.match() always resolves
+//     // but in case of success response will have value
+//     if (response !== undefined) {
+//         console.log("Not undefined");
+//         return response;
+//     } else {
+//         console.log("else");
+//         return fetch(event.request).then(function(response) {
+//             // response may be used only once
+//             // we need to save clone to put one copy in cache
+//             // and serve second one
+//             const responseClone = response.clone();
+
+//             caches.open(VERSION).then(function(cache) {
+//                 cache.put(event.request, responseClone);
+//             });
+//             return response;
+//         });
+//     }
